e885ec39593084ab14e9f2d9e2ab43e4587db868
Attila Ficsor <ficsorattila96@gmail.com>
2025-03-13 18:31:12 +0100

    Add copyright and licensing information

--- subprojects/logic/src/main/java/tools/refinery/logic/term/truthvalue/TruthValueConfidence.java
+++ subprojects/logic/src/main/java/tools/refinery/logic/term/truthvalue/TruthValueConfidence.java
@@ -1,3 +1,8 @@
+/*
+ * SPDX-FileCopyrightText: 2023-2024 The Refinery Authors <https://refinery.tools/>
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
 package tools.refinery.logic.term.truthvalue;
 
 import org.jetbrains.annotations.Nullable;
