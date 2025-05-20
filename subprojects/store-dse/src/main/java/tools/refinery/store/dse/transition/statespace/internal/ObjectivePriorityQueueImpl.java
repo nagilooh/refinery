b6527e13013a30680b3fc29d46dacb28afaac6d7
Attila Ficsor <ficsorattila96@gmail.com>
2025-05-20 15:26:22 +0200

    Add multi objective comparison

--- subprojects/store-dse/src/main/java/tools/refinery/store/dse/transition/statespace/internal/ObjectivePriorityQueueImpl.java
+++ subprojects/store-dse/src/main/java/tools/refinery/store/dse/transition/statespace/internal/ObjectivePriorityQueueImpl.java
@@ -16,23 +16,46 @@ import java.util.PriorityQueue;
 import java.util.Random;
 
 public class ObjectivePriorityQueueImpl implements ObjectivePriorityQueue {
-	public static final Comparator<VersionWithObjectiveValue> c1 = (o1, o2) -> Double.compare(
-			((ObjectiveValues.ObjectiveValue1) o1.objectiveValue()).value0(),
-			((ObjectiveValues.ObjectiveValue1) o2.objectiveValue()).value0());
+	public static final Comparator<VersionWithObjectiveValue> c1 = Comparator.comparingDouble(o -> ((ObjectiveValues.ObjectiveValue1) o.objectiveValue()).value0());
 	// TODO: support multi objective!
+	final Comparator<VersionWithObjectiveValue> comparator;
 	final PriorityQueue<VersionWithObjectiveValue> priorityQueue;
 
 	public ObjectivePriorityQueueImpl(List<Objective> objectives) {
 
-		if(objectives.size() == 1) {
-			this.priorityQueue = new PriorityQueue<>(c1);
-		} else {
-			throw new UnsupportedOperationException("Only single objective comparator is implemented currently!");
+		if(objectives.isEmpty()) {
+			throw new IllegalStateException("No objectives have been created");
 		}
+		if(objectives.size() == 1) {
+			this.comparator = c1;
+		}
+		else if(objectives.size() == 2) {
+			var paretoComparator = new ParetoComparator<VersionWithObjectiveValue>();
+			paretoComparator.add(Comparator.comparingDouble(o -> ((ObjectiveValues.ObjectiveValue2) o.objectiveValue()).value0()));
+			paretoComparator.add(Comparator.comparingDouble(o -> ((ObjectiveValues.ObjectiveValue2) o.objectiveValue()).value1()));
+			this.comparator = paretoComparator;
+		}
+		else {
+			var paretoComparator = new ParetoComparator<VersionWithObjectiveValue>();
+			for(var i = 0; i < objectives.size(); i++) {
+				int finalI = i;
+				paretoComparator.add(Comparator.comparingDouble(o -> o.objectiveValue().get(finalI)));
+			}
+			this.comparator = paretoComparator;
+
+
+		}
+		this.priorityQueue = new PriorityQueue<>(comparator);
 	}
+
+	public ObjectivePriorityQueueImpl(Comparator<VersionWithObjectiveValue> comparator) {
+		this.comparator = comparator;
+		this.priorityQueue = new PriorityQueue<>(comparator);
+	}
+
 	@Override
 	public Comparator<VersionWithObjectiveValue> getComparator() {
-		return c1;
+		return this.comparator;
 	}
 
 	@Override
