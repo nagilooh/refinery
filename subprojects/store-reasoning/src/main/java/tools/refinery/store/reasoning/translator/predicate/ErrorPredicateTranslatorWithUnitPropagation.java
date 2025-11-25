1889b80d881aad4f430572f56123fd49360780b0
Attila Ficsor <ficsorattila96@gmail.com>
2025-11-25 14:55:15 +0100

    Extend propagation rule generation for count > constant

--- subprojects/store-reasoning/src/main/java/tools/refinery/store/reasoning/translator/predicate/ErrorPredicateTranslatorWithUnitPropagation.java
+++ subprojects/store-reasoning/src/main/java/tools/refinery/store/reasoning/translator/predicate/ErrorPredicateTranslatorWithUnitPropagation.java
@@ -1,6 +1,7 @@
 package tools.refinery.store.reasoning.translator.predicate;
 
 import org.jetbrains.annotations.NotNull;
+import tools.refinery.logic.Constraint;
 import tools.refinery.logic.dnf.DnfClause;
 import tools.refinery.logic.dnf.Query;
 import tools.refinery.logic.dnf.RelationalQuery;
@@ -8,7 +9,7 @@ import tools.refinery.logic.literal.*;
 import tools.refinery.logic.term.ConstantTerm;
 import tools.refinery.logic.term.NodeVariable;
 import tools.refinery.logic.term.Variable;
-import tools.refinery.logic.term.abstractdomain.AbstractDomainBinaryTerm;
+import tools.refinery.logic.term.abstractdomain.*;
 import tools.refinery.logic.term.intinterval.IntInterval;
 import tools.refinery.logic.term.intinterval.IntIntervalTerms;
 import tools.refinery.logic.term.truthvalue.TruthValue;
@@ -128,101 +129,147 @@ public class ErrorPredicateTranslatorWithUnitPropagation extends PredicateTransl
 					}
 				} else if (literalToPropagate instanceof PartialCheckLiteral partialCheckLiteral) {
 					var term = partialCheckLiteral.getTerm();
-					if(term.getType().equals(TruthValue.class)) {
-						if (term instanceof AbstractDomainBinaryTerm abstractDomainBinaryTerm) {
-							var left = abstractDomainBinaryTerm.getLeft();
-							var right = abstractDomainBinaryTerm.getRight();
-							PartialCountTerm partialCountTerm;
-							ConstantTerm<IntInterval> constantTerm;
-							if (left instanceof PartialCountTerm pc && right instanceof ConstantTerm ct && ct.getType().equals(IntInterval.class)) {
-								partialCountTerm = pc;
-								constantTerm = (ConstantTerm<IntInterval>) ct;
-							} else if (left instanceof ConstantTerm ct && ct.getType().equals(IntInterval.class) && right instanceof PartialCountTerm pc) {
-								partialCountTerm = pc;
-								constantTerm = (ConstantTerm<IntInterval>) ct;
+					if(term.getType().equals(TruthValue.class) && term instanceof AbstractDomainBinaryTerm abstractDomainBinaryTerm) {
+						TermType termType;
+						switch (term) {
+						case AbstractDomainLessTerm abstractDomainLessTerm -> {
+							termType = TermType.LESS;
+						}
+						case AbstractDomainLessEqTerm abstractDomainLessEqTerm -> {
+							termType = TermType.LESS_EQ;
+							continue;
+						}
+						case AbstractDomainGreaterTerm abstractDomainGreaterTerm -> {
+							termType = TermType.GREATER;
+						}
+						case AbstractDomainGreaterEqTerm abstractDomainGreaterEqTerm -> {
+							termType = TermType.GREATER_EQ;
+							continue;
+						}
+						case AbstractDomainEqTerm  abstractDomainEqTerm -> {
+							termType = TermType.EQ;
+							continue;
+						}
+						case AbstractDomainNotEqTerm abstractDomainNotEqTerm -> {
+							termType = TermType.NOT_EQ;
+							continue;
+						}
+						default -> {continue;}
+						}
+						var left = abstractDomainBinaryTerm.getLeft();
+						var right = abstractDomainBinaryTerm.getRight();
+						PartialCountTerm partialCountTerm;
+						ConstantTerm<IntInterval> constantTerm;
+						var swapped = false;
+						if (left instanceof PartialCountTerm pc && right instanceof ConstantTerm ct && ct.getType().equals(IntInterval.class)) {
+							partialCountTerm = pc;
+							constantTerm = (ConstantTerm<IntInterval>) ct;
+						} else if (left instanceof ConstantTerm ct && ct.getType().equals(IntInterval.class) && right instanceof PartialCountTerm pc) {
+							partialCountTerm = pc;
+							constantTerm = (ConstantTerm<IntInterval>) ct;
+							swapped = true;
+						} else {
+							continue;
+						}
+						var target = partialCountTerm.getTarget();
+						if (target instanceof PartialRelation partialRelationTarget) {
+							String propagationName = "#propagateError#" + query.name() +
+									"#c" + clauseIndex + "l" + literalIndex;
+
+							List<NodeVariable> ruleParameters = new ArrayList<>();
+							List<NodeVariable> actionParameters = new ArrayList<>();
+
+							for (var argument : partialCountTerm.getArguments()) {
+								if (argument instanceof NodeVariable nodeVariable) {
+									if (!clause.positiveVariables().contains(argument)) {
+										var newPositiveVariable = Variable.of(propagationName + "#newPositiveVariable");
+										actionParameters.add(newPositiveVariable);
+										ruleParameters.add(newPositiveVariable);
+									} else {
+										actionParameters.add(nodeVariable);
+										if (!ruleParameters.contains(nodeVariable)) {
+											ruleParameters.add(nodeVariable);
+										}
+									}
+								} else {
+									throw new IllegalArgumentException("This argument is illegal");
+								}
+							}
+
+							// Precondition = []clause - {literalToPropagate} + <>literalToPropagate + ![]literalToPropagate
+							List<Literal> precondition = new ArrayList<>();
+							for (int i = 0; i < clause.literals().size(); i++) {
+								if (i != literalIndex) {
+									var lit = clause.literals().get(i);
+									switch (lit) {
+									case CallLiteral calLit -> {
+										precondition.add(calLit);
+										for (var arg : calLit.getArguments()) {
+											// Add node(arg) constraint
+											if (clause.positiveVariables().contains(arg)) {
+												precondition.add(nodePartialRelation.call(arg));
+											}
+										}
+									}
+									case ConstantLiteral constantLiteral -> precondition.add(constantLiteral);
+									case PartialCheckLiteral partialCheckLiteralCall ->
+											precondition.add(partialCheckLiteralCall);
+									default -> throw new UnsupportedOperationException();
+									}
+								}
+							}
+
+
+							Constraint countedConstraint;
+							final TruthValue toMerge;
+							if (termType == TermType.LESS && !swapped) {
+								countedConstraint = ModalConstraint.of(Modality.MAY, Concreteness.PARTIAL,
+										partialRelationTarget);
+								toMerge = TruthValue.TRUE;
+							} else if (termType == TermType.LESS) {
+								countedConstraint = ModalConstraint.of(Modality.MUST, Concreteness.PARTIAL,
+										partialRelationTarget);
+								toMerge = TruthValue.FALSE;
+							} else if (termType == TermType.GREATER && !swapped) {
+								countedConstraint = ModalConstraint.of(Modality.MUST, Concreteness.PARTIAL,
+										partialRelationTarget);
+								toMerge = TruthValue.FALSE;
+							} else if (termType == TermType.GREATER) {
+								countedConstraint = ModalConstraint.of(Modality.MAY, Concreteness.PARTIAL,
+										partialRelationTarget);
+								toMerge = TruthValue.TRUE;
 							} else {
 								continue;
 							}
-							var target = partialCountTerm.getTarget();
-							if (target instanceof PartialRelation partialRelationTarget) {
-								String propagationName = "#propagateError#" + query.name() +
-										"#c" + clauseIndex + "l" + literalIndex;
 
-								List<NodeVariable> ruleParameters = new ArrayList<>();
-								List<NodeVariable> actionParameters = new ArrayList<>();
+							var countTerm = new PartialCountTerm(countedConstraint, partialCountTerm.getArguments());
+							PartialCheckLiteral countPartialCheckLiteral = new PartialCheckLiteral(IntIntervalTerms.eq(countTerm,
+									constantTerm));
 
-								for (var argument : partialCountTerm.getArguments()) {
-									if (argument instanceof NodeVariable nodeVariable) {
-										if (!clause.positiveVariables().contains(argument)) {
-											var newPositiveVariable = Variable.of(propagationName + "#newPositiveVariable");
-											actionParameters.add(newPositiveVariable);
-											ruleParameters.add(newPositiveVariable);
-										} else {
-											actionParameters.add(nodeVariable);
-											if (!ruleParameters.contains(nodeVariable)) {
-												ruleParameters.add(nodeVariable);
-											}
-										}
-									} else {
-										throw new IllegalArgumentException("This argument is illegal");
-									}
-								}
-
-								// Precondition = []clause - {literalToPropagate} + <>literalToPropagate + ![]literalToPropagate
-								List<Literal> precondition = new ArrayList<>();
-								for (int i = 0; i < clause.literals().size(); i++) {
-									if (i != literalIndex) {
-										var lit = clause.literals().get(i);
-										switch (lit) {
-										case CallLiteral calLit -> {
-											precondition.add(calLit);
-											for (var arg : calLit.getArguments()) {
-												// Add node(arg) constraint
-												if (clause.positiveVariables().contains(arg)) {
-													precondition.add(nodePartialRelation.call(arg));
-												}
-											}
-										}
-										case ConstantLiteral constantLiteral -> precondition.add(constantLiteral);
-										case PartialCheckLiteral partialCheckLiteralCall ->
-												precondition.add(partialCheckLiteralCall);
-										default -> throw new UnsupportedOperationException();
-										}
-									}
-								}
-
-								var mayConstraint = ModalConstraint.of(Modality.MAY, Concreteness.PARTIAL,
-										partialRelationTarget);
-								var countMayTerm = new PartialCountTerm(mayConstraint, partialCountTerm.getArguments());
-								var mayEqPartialCheckLiteral =
-										new PartialCheckLiteral(IntIntervalTerms.eq(countMayTerm, constantTerm));
-								precondition.add(mayEqPartialCheckLiteral);
+							precondition.add(countPartialCheckLiteral);
 
 
-								CallLiteral callLiteral = new CallLiteral(CallPolarity.POSITIVE,
-										partialRelationTarget, new ArrayList<>(actionParameters));
-								precondition.add(addModality(callLiteral, Modality.MAY));
-								precondition.add(Literals.not(addModality(callLiteral, Modality.MUST)));
+							CallLiteral callLiteral = new CallLiteral(CallPolarity.POSITIVE,
+									partialRelationTarget, new ArrayList<>(actionParameters));
+							precondition.add(addModality(callLiteral, Modality.MAY));
+							precondition.add(Literals.not(addModality(callLiteral, Modality.MUST)));
 
-								final TruthValue toMerge = TruthValue.TRUE;
+							var preconditionQuery = Query.of(propagationName + "#precondition", builder -> builder
+									.parameters(ruleParameters)
+									.clause(precondition));
 
-								var preconditionQuery = Query.of(propagationName + "#precondition", builder -> builder
-										.parameters(ruleParameters)
-										.clause(precondition));
+							var rule = Rule.of(propagationName, builder -> builder
+									.parameters(ruleParameters)
+									.clause(must(preconditionQuery.call(CallPolarity.POSITIVE, ruleParameters)))
+									.action(PartialActionLiterals.merge(partialRelationTarget, toMerge,
+											actionParameters)));
+							propagationBuilder.rule(rule);
 
-								var rule = Rule.of(propagationName, builder -> builder
-										.parameters(ruleParameters)
-										.clause(must(preconditionQuery.call(CallPolarity.POSITIVE, ruleParameters)))
-										.action(PartialActionLiterals.merge(partialRelationTarget, toMerge,
-												actionParameters)));
-								propagationBuilder.rule(rule);
-
-								var concretizationRule = Rule.of(propagationName + "#concretize", builder -> builder
-										.parameters(ruleParameters)
-										.clause(candidateMust(preconditionQuery.call(CallPolarity.POSITIVE, ruleParameters)))
-										.action(PartialActionLiterals.merge(partialRelationTarget, toMerge, actionParameters)));
-								propagationBuilder.concretizationRule(concretizationRule);
-							}
+							var concretizationRule = Rule.of(propagationName + "#concretize", builder -> builder
+									.parameters(ruleParameters)
+									.clause(candidateMust(preconditionQuery.call(CallPolarity.POSITIVE, ruleParameters)))
+									.action(PartialActionLiterals.merge(partialRelationTarget, toMerge, actionParameters)));
+							propagationBuilder.concretizationRule(concretizationRule);
 						}
 					}
 				}
---
+++ subprojects/store-reasoning/src/main/java/tools/refinery/store/reasoning/translator/predicate/TermType.java
@@ -0,0 +1,10 @@
+package tools.refinery.store.reasoning.translator.predicate;
+
+public enum TermType {
+	LESS,
+	GREATER,
+	LESS_EQ,
+	GREATER_EQ,
+	EQ,
+	NOT_EQ;
+}
