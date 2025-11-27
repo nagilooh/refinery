bdd7afb6a145994269f4189efb3b0ed8a763c673
Attila Ficsor <ficsorattila96@gmail.com>
2025-11-27 15:17:21 +0100

    Set propagated preds as mutable

--- subprojects/language-semantics/src/main/java/tools/refinery/language/semantics/ModelInitializer.java
+++ subprojects/language-semantics/src/main/java/tools/refinery/language/semantics/ModelInitializer.java
@@ -803,7 +803,7 @@ public class ModelInitializer {
 			translator = new ErrorPredicateTranslatorWithUnitPropagation(partialRelation, query, parameterTypes,
 					supersets, mutable, defaultValue, nodePartialRelation);
 		} else {
-			translator = new PredicateTranslator(partialRelation, query, parameterTypes, supersets, true,
+			translator = new PredicateTranslator(partialRelation, query, parameterTypes, supersets, mutable,
 					defaultValue);
 		}
 
