d2fbab03678d38183447c35bb9dc924450ea6abb
Attila Ficsor <ficsorattila96@gmail.com>
2025-12-19 12:39:24 +0100

    Update uncertainty measurement

--- subprojects/measurement/src/main/java/tools/refinery/measurement/CountUnknown.java
+++ subprojects/measurement/src/main/java/tools/refinery/measurement/CountUnknown.java
@@ -7,32 +7,103 @@ import tools.refinery.logic.term.truthvalue.TruthValue;
 import tools.refinery.store.reasoning.representation.PartialRelation;
 
 import java.io.IOException;
+import java.nio.file.DirectoryStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.List;
 
 public class CountUnknown {
+	static List<String> ignored = Arrays.asList("entryInRegion," +
+					"noEntryInRegion",
+			"multipleEntryInRegion",
+			"incomingToEntry",
+			"noOutgoingTransitionFromEntry",
+			"multipleTransitionFromEntry",
+			"outgoingFromExit",
+			"outgoingFromFinal",
+			"stateInRegion",
+			"noStateInRegion",
+			"choiceHasNoOutgoing",
+			"choiceHasNoIncoming",
+			"sameStraightDiverging",
+			"noDiverging",
+			"noStraight",
+			"tooManyConncetions",
+			"selfLoop",
+			"TrackElement",
+			"underconnectedSwitch",
+			"switchWithTwoConnections",
+			"sensorWithoutElement",
+			"sensorMonitorsMultipleSwitches",
+			"duplicateMonitors",
+			"emptyRegion",
+			"posLength",
+			"switchMonitored",
+			"routeSensor",
+			"switchSet",
+			"connectedSegments",
+			"semaphoreNeighbor",
+			"HC1",
+			"HC2c",
+			"HC2",
+			"SC1",
+			"hasCancle",
+			"SC2",
+			"computed");
+
 	public static void main(String[] args) throws IOException {
-		Problem parsedProblem = StandaloneRefinery.getProblemLoader().loadFile("test-output/solution.refinery");
-		ModelSemantics semantics = StandaloneRefinery.getSemanticsFactory().tryCreateSemantics(parsedProblem);
-		var trace = semantics.getProblemTrace();
+		Path dir = Paths.get("test_output");
 
-		int unknownCount = 0;
-		for (var entry : trace.getRelationTrace().entrySet()) {
-			System.out.println(entry.getKey().getName());
-			PartialRelation partialRelation;
-			try {
-				partialRelation = entry.getValue().asPartialRelation();
+		if (!Files.isDirectory(dir)) {
+			System.err.println("Directory not found: " + dir);
+			return;
+		}
 
-			}
-			catch (IllegalStateException e) {
-				continue;
-			}
-			var cursor = semantics.getPartialInterpretation(partialRelation).getAll();
+		try (DirectoryStream<Path> stream = Files.newDirectoryStream(dir, "*.problem")) {
+			for (Path path : stream) {
+				System.out.println("Processing file: " + path.getFileName());
+				Problem parsedProblem = StandaloneRefinery.getProblemLoader().loadFile(path.toString());
+				ModelSemantics semantics = StandaloneRefinery.getSemanticsFactory().tryCreateSemantics(parsedProblem);
+				var trace = semantics.getProblemTrace();
 
-			while (cursor.move()) {
-				if (cursor.getValue() == TruthValue.UNKNOWN) {
-					unknownCount++;
+				int unknownCount = 0;
+				int allCount = 0;
+
+				for (var entry : trace.getRelationTrace().entrySet()) {
+					var name = entry.getKey().getName();
+					if (ignored.contains(name)) {
+						continue;
+					}
+					System.out.println(name);
+					PartialRelation partialRelation;
+					try {
+						partialRelation = entry.getValue().asPartialRelation();
+					} catch (IllegalStateException e) {
+						continue;
+					}
+					var cursor = semantics.getPartialInterpretation(partialRelation).getAll();
+
+					while (cursor.move()) {
+						allCount++;
+						if (cursor.getValue() == TruthValue.UNKNOWN) {
+							unknownCount++;
+						}
+					}
 				}
+
+				System.out.println("Unknown count: " + unknownCount);
+
+				// append result for this file to CSV
+				Path csv = Paths.get("unknown_counts.csv");
+				if (!Files.exists(csv)) {
+				    Files.writeString(csv, "file,allCount,unknownCount\n");
+				}
+				String csvLine =
+						path.getFileName().toString() + "," + allCount + "," + unknownCount + System.lineSeparator();
+				Files.writeString(csv, csvLine, java.nio.file.StandardOpenOption.CREATE, java.nio.file.StandardOpenOption.APPEND);
 			}
 		}
-		System.out.println("Unknown count: " + unknownCount);
 	}
 }
