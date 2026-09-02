5120ce40f9aad783402bd9e8ebed15695f8e9201
Attila Ficsor <ficsorattila96@gmail.com>
2026-09-02 21:04:18 +0200

    Update copyright

--- subprojects/store-transition-system/src/main/java/tools/refinery/store/transition/system/strategy/TransitionSystemExplorer.java
+++ subprojects/store-transition-system/src/main/java/tools/refinery/store/transition/system/strategy/TransitionSystemExplorer.java
@@ -1,3 +1,8 @@
+/*
+ * SPDX-FileCopyrightText: 2026 The Refinery Authors <https://refinery.tools/>
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
 package tools.refinery.store.transition.system.strategy;
 
 import tools.refinery.store.dse.propagation.PropagationAdapter;
